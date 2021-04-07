e555c86ff96ea7b89220a39830b8f59fee2c10ca
EgiVilza <egivilza@gmail.com>
2021-04-07 17:01:46 -0500

    updated service-worker.js code

--- public/service-worker.js
+++ public/service-worker.js
@@ -68,9 +68,14 @@ self.addEventListener("fetch", function(evt) {
     }
   
     evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
+      fetch(evt.request).catch(function () {
+        return caches.match(evt.request).then(function (response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
         });
       })
     );
